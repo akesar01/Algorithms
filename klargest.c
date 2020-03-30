3d0ab665f05fe8b9449abbf364ee831a11156689
mrawk <56121226+mrawk@users.noreply.github.com>
2020-03-30 22:27:13 +0530

    update more efficient sol

--- klargest.c
+++ klargest.c
@@ -1,50 +1,54 @@
 #include<stdio.h>
-#include<conio.h>
 #include<time.h>
 int startt,endt,timesort;
-void  ss(int a[],int n);
-int k,n;
+void k_lar(int arr[],int size,int k)
+{
+for(int i=1;i<size;i++)
+{
+    int temp=arr[i];
+    int j;
+    j=i;
+    while(j>0 && temp<arr[j-1])
+    {
+        arr[j]=arr[j-1];
+        j--;
+        
+    }
+    arr[j]=temp;
+    
+    
+}
+ 
+for(int l=0;l<size;l++)
+{
+    printf("%d ",arr[l]);
+}
+printf("\n");
+printf("The %d largest element are : ",k);
+for(int i=size-1;i>=0;i--)
+{
+    
+    int temp =arr[i];
+    if(temp !=arr[i-1] && k>0)
+    {
+        printf("%d " ,arr[i]);
+        k--;
+        
+    }
+    
+}
+printf("\n");
+printf("Time taken is : ");
+    endt=clock();
+    timesort=((endt-startt)/2);
+    printf("%d",timesort);
+
+
+
+}
 void main()
 {
-    int a[10];
-    printf("enter the no. of elements");
-    scanf("%d",&n);
-    printf("enter the elements into the array");
-    for(int i=0;i<n;i++)
-    {
-        scanf("%d",&a[i]);
-    }
-    printf("enter the value of k:");
-    scanf("%d",&k);
+    int arr[]={8,2,8,8,1};
     startt=clock();
-    ss(a,n);
-    endt=clock();
-    timesort=endt-startt;
-    printf("\nthe time taken = %d",timesort);
-}
-void ss(int a[],int n)
-{
-    int small,pos,temp;
-    for(int i=0;i<(n-1);i++)
-    {
-        pos=i;
-        small=a[i];
-        for(int j=i+1;j<n;j++)
-        {
-            if(small>a[j]){
-                small=a[j];
-                pos=j;}
-        }
-        temp=a[i];
-        a[i]=a[pos];
-        a[pos]=temp;
-    }
-    printf("the sorted elements are :");
-    for(int i=0;i<n;i++)
-        printf("%d ",a[i]);
-
-    printf("\nthe largest elements are : ");
-    for(int i=0;i<k;i++)
-        printf("%d ",a[(n-1)-i]);
-
+    k_lar(arr, 5,3);
 }
